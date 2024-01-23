d5f0213cc9c233c8d08c45e6b9c350943e0b89cb
rodriguesathena <athenabrodrigues@gmail.com>
2024-01-22 19:41:20 -0500

    creating gen_000
    proposal file created

--- proposal/json_gen_000.py
+++ proposal/json_gen_000.py
@@ -16,7 +16,7 @@ data_to_save = \
     {
         # -----------------------------------------------------------------------------------------------------------------------
         "Version":
-            """116""",
+            """000""",
         # -----------------------------------------------------------------------------------------------------------------------
         "Year":
             """2024""",
@@ -25,21 +25,42 @@ data_to_save = \
             """Spring""",
         # -----------------------------------------------------------------------------------------------------------------------
         "project_name":
-            """Trends in Immigration in the United States""",
+            """Mapping Deprived Areas in Low and Middle Income Countries (LMIC) - Part 1""",
         # -----------------------------------------------------------------------------------------------------------------------
         "Objective":
             """ 
-            The aim of this project is to track immigration rates into the United States
+            The goal of this project is to develop a methodology to map of deprived areas using a range of geospatial data
+            at approximately 100m grid cells. This will help cities understand the spatial pattern of Sustainable Development 
+            Goal 1 (Poverty) and Sustainable Development Goal 11 (sustainable cities and Communities). Want to use a
+            combination of open source geospatial data and satellite imagery to map variations in poverty. Allow for
+            comparisons between cities and determine if models made in one city can be used in another city. 
+            This project could possibly be funded by the European Space Agency (for 8 cities) and theoretically
+            could be expanded to the entire developing world.
             """,
         # -----------------------------------------------------------------------------------------------------------------------
         "Dataset":
             """
-             
+            Dr. Ryan is going to provide us the data and the location of it. 
+
+            We have a geospatial data for these three cities: Lagos Nigeria, Accra Ghana and Nairobi Kenya. Each city there is a 
+            series of covariates at 100m grid cells. There are 166 individual tif files that are aligned to the same grid for 
+            the cities. There are approximately 50-60 for each city. These data sets represent infrastructure, social economic 
+            status (SES), facilities and services, physical hazards and assets and others. The number of covariates varies by 
+            city, but there is a set that is common to all of the cities.  
+
+            For each city we are working on extracting Sentinel 2 imagery (10m spatial resolution) for each city. We are running 
+            contextual features for each city. This produces 144 layers from the imagery that map spatial patterns and estimates 
+            of vegetation. These features are calculated on the same grid and in such a way that a 10x10 group of pixels matches 
+            each 100m grid cell. Imagery is collected from Google Earth Engine (Training and validation Data). For Lagos and Accra
+            we have created a training and validation data set at the grid cell level. Training data is Deprived residential,
+            Other Built, and non-built. Model Attributes: Want to be able to model deprivation estimate at the 100m grid cell 
+            level. Test different models to be able to do this. Also, estimate of degree of deprivation, produce a scale of 
+            deprivation so that city governments can set this level themselves and play around with the models.  
             """,
         # -----------------------------------------------------------------------------------------------------------------------
         "Rationale":
             """
-            
+            This project is going to help agencies to tackle poverty and help countries.
             """,
         # -----------------------------------------------------------------------------------------------------------------------
         "Approach":
@@ -80,7 +101,7 @@ data_to_save = \
         "Proposed by email": "arodrigues@gwu.edu",
         "instructor": "Edwin Lo",
         "instructor_email": "edwinlo@gwu.edu",
-        "github_repo": "https://github.com/rodriguesathena/24Spr_ARodrigues_Capstone",
+        "github_repo": "https://github.com/amir-jafari/Capstone",
         # -----------------------------------------------------------------------------------------------------------------------
     }
 os.makedirs(
