d8430f6c3e8d6bfb0a31a12ed62525f8a22bc028
rodriguesathena <athenabrodrigues@gmail.com>
2024-01-29 18:32:31 -0500

    proposal html

--- proposals/Proposal_gen/Proposals/202401/000/json_gen_000.py
+++ proposals/Proposal_gen/Proposals/202401/000/json_gen_000.py
@@ -30,23 +30,23 @@ data_to_save = \
         "Objective":
             """ 
             The goal of this project is to analyze women's progression internationally in the political realm over 100 years (1900-2000).
-            This will aid in understanding how, over time, women’s access to civil liberties, participation in society, and representation in politics have changed. 
+            This will aid in understanding how, over time, womes access to civil liberties, participation in society, and representation in politics have changed. 
             By tracking whether women have the right to vote in each nation in line with the political empowerment index it can be determined if there is a correlation between the two. 
-            It is important to understand the struggle for women’s rights and gender equality in history to ensure they are not taken for granted or chipped away. 
+            It is important to understand the struggle for women's rights and gender equality in history to ensure they are not taken for granted or chipped away. 
             """,
         # -----------------------------------------------------------------------------------------------------------------------
         "Dataset":
             """
-            The current datasets, Women’s Empowerment Index and Women’s Universal Right to Vote, come from Ourworldindata.
+            The current datasets, Women's Empowerment Index and Womens Universal Right to Vote, come from Ourworldindata.
 	
-	        The Women’s Political Empowerment Index (WPEI) is calculated by V-Dem Institute, a think tank that studies democracy and other government regimes around the world. 
-            A combination of the Women Civil Liberties Index, Women Civil Society Participation Index, and Women Political Participation Index, the Women’s Political Empowerment Index provides a quantitative analysis of women’s rights and aids in understanding the development or destruction of democracy equal civil rights is seen as a necessity to transition to democracy. 
+	        The Women's Political Empowerment Index (WPEI) is calculated by V-Dem Institute, a think tank that studies democracy and other government regimes around the world. 
+            A combination of the Women Civil Liberties Index, Women Civil Society Participation Index, and Women Political Participation Index, the Women Political Empowerment Index provides a quantitative analysis of women's rights and aids in understanding the development or destruction of democracy equal civil rights is seen as a necessity to transition to democracy. 
             The data breaks down the WPEI by continent and country from 1989 to 2022 tracking the absolute and relative changes of the central estimates and both bounds.
         	
-            The Women's Universal Right to Vote dataset tracks whether women’s suffrage is present in a country using a system of 0 and 1 (0 for no, 1 for yes). 
+            The Women's Universal Right to Vote dataset tracks whether womens suffrage is present in a country using a system of 0 and 1 (0 for no, 1 for yes). 
             This data is broken down by country and records the right to vote from 1789 to 2022.
 	       
-            Because both of the datasets span the same range of time, it should be easy to compare and combine datasets without loss of data or confusion. The other datasets that created the Women’s Empowerment Index are being looked for to be included for better analysis
+            Because both of the datasets span the same range of time, it should be easy to compare and combine datasets without loss of data or confusion. The other datasets that created the Women's Empowerment Index are being looked for to be included for better analysis
 
            """,
         # -----------------------------------------------------------------------------------------------------------------------
@@ -72,7 +72,7 @@ data_to_save = \
             This a rough time line for this project:  
 
             Feb. 12: Mapping Right to Vote over time
-	        Feb. 19: Mapping Women’s Empowerment Index over time
+	        Feb. 19: Mapping Women Empowerment Index over time
 	        Mar. 4: comparing indices (components of WPEI)
 	        Mar. 18: Testing relationships
 	        Apr. 1: Pre-suffrage, suffrage, post-suffrage graphics
